d579db8eb987333f37c56cbeae44a8bbb6ffbc56
Manju <manju@vedapixel.com>
2026-03-10 13:41:35 +0530

    feat:develop first part of payment and finance page

---
+++ src/app/(dashboard)/paymentandfinance/page.tsx
@@ -0,0 +1,9 @@
+import { PFScreen } from "@/features/paymentandfinance/screen/PFScreen";
+
+export default function PFPage() {
+  return (
+    <div className="px-8 py-5    min-h-screen bg-[#F8F9FA]">
+      <PFScreen></PFScreen>
+    </div>
+  );
+}
--- src/shared/excomponent/charts/PieChart.tsx
+++ src/shared/excomponent/charts/PieChart.tsx
@@ -123,3 +123,133 @@ export function ChartPieDonut() {
     </Card>
   );
 }
+
+
+
+
+// "use client";
+
+// import * as React from "react";
+// import { TrendingUp } from "lucide-react";
+// import { Label, Pie, PieChart } from "recharts";
+
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardFooter,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import {
+//   ChartContainer,
+//   ChartTooltip,
+//   ChartTooltipContent,
+//   type ChartConfig,
+// } from "@/components/ui/chart";
+
+// export const description = "A donut chart with text";
+
+// const chartData = [
+//   { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+//   { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+//   { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
+//   { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+// ];
+
+// const chartConfig = {
+//   visitors: {
+//     label: "Visitors",
+//   },
+//   chrome: {
+//     label: "Chrome",
+//     color: "#38678C",
+//   },
+//   safari: {
+//     label: "Safari",
+//     color: "#CBDEEC",
+//   },
+//   firefox: {
+//     label: "Firefox",
+//     color: "#4889BC",
+//   },
+//   edge: {
+//     label: "Edge",
+//     color: "#8DAFD1",
+//   },
+//   other: {
+//     label: "Other",
+//     color: "var(--chart-5)",
+//   },
+// } satisfies ChartConfig;
+
+// export function ChartPieDonut() {
+//   const totalVisitors = React.useMemo(() => {
+//     return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
+//   }, []);
+
+//   return (
+//     <Card className="flex flex-col  bg-transparent shadow-none  border-0">
+//       {/* <CardHeader className="items-center pb-0">
+//         <CardTitle>Pie Chart - Donut with Text</CardTitle>
+//         <CardDescription>January - June 2024</CardDescription>
+//       </CardHeader> */}
+//       <CardContent className="flex-1 p-0">
+//         <ChartContainer
+//           config={chartConfig}
+//           className="w-[330px] h-[330px]"
+//         >
+//           <PieChart margin={{ top: 0, right: 0, bottom: 0, left: 0 }} className="">
+//             <ChartTooltip
+//               cursor={false}
+//               content={<ChartTooltipContent hideLabel />}
+//             />
+//             <Pie
+//               data={chartData}
+//               dataKey="visitors"
+//               nameKey="browser"
+//               paddingAngle={5}
+//               cornerRadius={5}
+//               stroke="none"
+//               innerRadius="60%"
+//               outerRadius="95%"
+//               strokeWidth={0}
+//             >
+//               <Label
+//                 content={({ viewBox }) => {
+//                   if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+//                     return (
+//                       <text
+//                         x={viewBox.cx}
+//                         y={viewBox.cy}
+//                         textAnchor="middle"
+//                         dominantBaseline="middle"
+//                       >
+//                         {/* Label moved UP by subtracting 16 from cy */}
+//                         <tspan
+//                           x={viewBox.cx}
+//                           y={(viewBox.cy || 0) - 16}
+//                           className="fill-neutral-1 text-sm"
+//                         >
+//                           Totale Sale
+//                         </tspan>
+//                         {/* Amount moved DOWN by adding 16 to cy */}
+//                         <tspan
+//                           x={viewBox.cx}
+//                           y={(viewBox.cy || 0) + 16}
+//                           className="fill-neutral-2 text-[20px] 2xl:text-[20px] font-semibold"
+//                         >
+//                           $13,44,000
+//                         </tspan>
+//                       </text>
+//                     );
+//                   }
+//                 }}
+//               />
+//             </Pie>
+//           </PieChart>
+//         </ChartContainer>
+//       </CardContent>
+//     </Card>
+//   );
+// }
