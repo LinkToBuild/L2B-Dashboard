2b688b72f0a5c44591c6b9e0c6fbaace5dea7464
adeshM07 <adesh.m@vedapixel.com>
2026-03-20 13:07:27 +0530

    chore: Changed the file name of main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 import CustomerManagementPage from "./(dashboard)/customermanagement/page";
 
-export default function Home() {
+export default function page() {
   // It will redirect to CustomerManagement By default
   return redirect("/customermanagement");
 }
