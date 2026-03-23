eef6bcf499f7fe766b569fb46162a7fb0716d191
adeshM07 <adesh.m@vedapixel.com>
2026-03-23 15:45:27 +0530

    fix: resolve ReferenceError by using server-side redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Image from "next/image";
 import { redirect } from "next/navigation";
-import CustomerManagementPage from "./(dashboard)/customermanagement/page";
 
-export default function page() {
-  return redirect("/customermanagement");
+export default function Page() {
+  redirect("/customermanagement");
 }
