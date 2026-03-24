56f199bcaa7435c64d4152c4d19623d7d2ec1ed8
adeshM07 <adesh.m@vedapixel.com>
2026-03-24 12:26:24 +0530

    fix(inventory): add Suspense boundary to page route
    - Wrapped InventoryScreen in a React Suspense component.
    
    - Prevents Next.js static prerendering build crashes caused by useSearchParams.

--- src/app/(dashboard)/inventorymanagement/page.tsx
+++ src/app/(dashboard)/inventorymanagement/page.tsx
@@ -1,9 +1,17 @@
 import { InventoryScreen } from "@/features/inventorymanagement/screen/InventoryScreen";
+import { Suspense } from "react";
 
 export default function InventoryManagementPage() {
   return (
     <div className="px-8 py-5    min-h-screen bg-[#F8F9FA]">
-      <InventoryScreen></InventoryScreen>
+     
+       <Suspense
+              fallback={
+                <div className="p-8 text-neutral-3">Loading dashboard...</div>
+              }
+            >
+               <InventoryScreen></InventoryScreen>
+            </Suspense>
     </div>
   );
 }
